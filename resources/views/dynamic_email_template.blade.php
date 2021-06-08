083342f9e240b3c14505386c6274b8f7de1395d8
KennethNdungu <mathari580@gmail.com>
2021-06-08 16:31:02 +0300

    Revert "Upload files"
    This reverts commit 920a75227a5178793d26cbc2f2e254eb5cdeaa83.

--- resources/views/dynamic_email_template.blade.php
+++
@@ -1,6 +0,0 @@
-<p>Hi, This is <b>{{ $data['name'] }}</b> from <b>{{ $data['location'] }}</b></p>
-<hr>
-<p>Phone number: {{ $data['phonenumber'] }}</p>
-<p>Email: {{ $data['email'] }}</p>
-<hr>
-<p>{{ $data['message'] }}.</p>
--- resources/views/inc/header.blade.php
+++
@@ -1,8 +0,0 @@
-<div  style="font-size:16px;background-color:#F5F5F5">
-    <div class="container" >
-        <div class="col-md-4"><span class="glyphicon glyphicon-earphone"></span>  <a href="tel:+254722747545" style="text-decoration:none;color:#000">+254 722 747 545</a></div>
-        <div class="col-md-4"><span class="glyphicon glyphicon-envelope"></span>  <a href="mailto: info@oaksandpines.co.ke" style="text-decoration:none;color:#000">info@oaksandpines.co.ke</a></div>
-        <div class="col-md-4"><span class='fab fa-instagram'></span>  <a href="https://www.instagram.com/oaksandpinesltd/" style="text-decoration:none;color:#000">oaksandpinesltd</a></div>
-                                          
-    </div>
-</div>
