ba79685f0d41c70983923b0cfb86181e74eba071
KennethNdungu <mathari580@gmail.com>
2021-08-11 17:36:15 +0300

    created Model & Controller for Posts

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('posts','PostsController');
+
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
