3cd0a5c24f706aa9ee62a429c8193e889587d569
KennethNdungu <mathari580@gmail.com>
2021-09-06 16:42:04 +0300

    Created Partial Components for Guest Layout
    -head section
    -navbar
    -footer

---
+++ resources/views/components/partials/head.blade.php
@@ -0,0 +1,18 @@
+<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>@yield('title','Oaks & Pines Landscaping LTD')</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        {{-- <link rel="stylesheet" href="{{ mix('css/app.css') }}"> --}}
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+
+        <!-- Livewire -->
+        @livewireStyles
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,24 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <x-partials.head/>
 
-        <title>@yield('title','Oaks & Pines Landscaping LTD')</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
-        {{-- <link rel="stylesheet" href="{{ mix('css/app.css') }}"> --}}
-        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-
-        <!-- Livewire -->
-        @livewireStyles
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
@@ -26,9 +10,7 @@
         </div>
 
         <footer>
-            <div>
-                <h2>Copyright &copy;</h2>
-            </div>
+            <x-partials.footer/>
         </footer>
 
         @livewireScripts
