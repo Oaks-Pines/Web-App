09c96b366de4825db40d07e3b68edacf25ada21c
KennethNdungu <mathari580@gmail.com>
2021-10-04 23:54:35 +0300

    Created Categories Table UI

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -2,6 +2,36 @@
     
      
    <p>Categories</p>
+   <section class="container mx-auto p-6 font-mono">
+    <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
+      <div class="w-full overflow-x-auto">
+        <table class="w-full">
+          <thead>
+            <tr class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
+              <th class="px-4 py-3">ID</th>
+              <th class="px-4 py-3">NAME</th>
+              <th class="px-4 py-3">SUB CATEGORIES</th>
+              <th class="px-4 py-3">DATE CREATED</th>
+              <th class="px-4 py-3">DATE UPDATED</th>
+              <th class="px-4 py-3">ACTIONS</th>
+            </tr>
+          </thead>
+          <tbody class="bg-white">
+            <tr class="text-gray-700">
+              <td class="px-4 py-3 border">1</td>
+              <td class="px-4 py-3 text-ms font-semibold border">22</td>
+              <td class="px-4 py-3 text-xs border">were</td>
+              <td class="px-4 py-3 text-sm border">6/4/2000</td>
+              <td class="px-4 py-3 text-sm border">6/4/2000</td>
+              <td class="px-4 py-3 text-sm border">6/4/2000</td>
+            </tr>
+            
+          </tbody>
+        </table>
+        
+      </div>
+    </div>
+  </section>
      
    
  </x-app-layout>
