efc8d9c03faf9672d5c9320a81c6d103fe191a59
KennethNdungu <mathari580@gmail.com>
2021-10-04 23:40:20 +0300

    Created Category Link

--- resources/views/components/partials/sidebarnav.blade.php
+++ resources/views/components/partials/sidebarnav.blade.php
@@ -56,14 +56,14 @@
                   />
                 </svg>
               </a>
-              <!-- Message link -->
+              <!-- Category link -->
               <!-- Active classes "bg-primary text-white" -->
               <!-- inActive classes "bg-primary-50 text-primary-lighter" -->
               <a
-                href="#"
+                href="/dashboard/category"
                 class="p-2 transition-colors duration-200 rounded-full text-primary-lighter bg-primary-50 hover:text-primary hover:bg-primary-100 dark:hover:text-light dark:hover:bg-primary-dark dark:bg-dark focus:outline-none focus:bg-primary-100 dark:focus:bg-primary-dark focus:ring-primary-darker"
               >
-                <span class="sr-only">Messages</span>
+                <span class="sr-only">Categories</span>
                 <svg
                   class="w-7 h-7"
                   xmlns="http://www.w3.org/2000/svg"
