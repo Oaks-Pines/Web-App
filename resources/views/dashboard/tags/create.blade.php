bf3e0fb863b2fcf3133441fa419ff95679c85a81
KennethNdungu <mathari580@gmail.com>
2021-10-20 13:13:21 +0300

    Updated Create Tag UI

--- resources/views/dashboard/tags/create.blade.php
+++ resources/views/dashboard/tags/create.blade.php
@@ -1,9 +1,17 @@
 <x-app-layout>
-    
-    
-       
-     
-     
-       
-     
-   </x-app-layout>
+    <h1>Create Tag</h1>
+    <form action="{{route('tags.store')}}" method="POST" class="m-2">
+        @csrf
+
+        <div class="mb-4">
+            <label class="block text-white-700 text-sm font-bold mb-2" for="name">
+              Name
+            </label>
+            <input name="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="name">
+            <span class="mt-2 text-xs text-gray-500">Maximum 200 Characters</span>
+            <x-jet-input-error for="name" class="mt-2"/>
+          </div>
+
+          <x-jet-button>Create</x-jet-button>
+    </form>
+ </x-app-layout>
